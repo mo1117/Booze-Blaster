2ae874fbe31cd00487b0793fb4b5aae052f83f41
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2024-05-12 16:54:22 +0200

    Remove unneccessary imports

--- app/src/main/java/com/boozeblaster/Test.kt
+++ app/src/main/java/com/boozeblaster/Test.kt
@@ -1,10 +1,5 @@
 package com.boozeblaster
 
-import com.boozeblaster.generators.taskFactories.CommonTaskFactory
-import com.boozeblaster.generators.taskFactories.IndividualTaskFactory
-import com.boozeblaster.generators.taskFactories.VersusTaskFactory
-import com.boozeblaster.models.Player
-
 fun main() {
 
 }

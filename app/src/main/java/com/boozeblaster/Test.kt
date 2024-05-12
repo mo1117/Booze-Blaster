42e5b7f1d6fedb9f5a0d1b4e410381f8c8948242
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2024-05-12 16:17:55 +0200

    Remove unnecessary test code

--- app/src/main/java/com/boozeblaster/Test.kt
+++ app/src/main/java/com/boozeblaster/Test.kt
@@ -6,12 +6,5 @@ import com.boozeblaster.generators.taskFactories.VersusTaskFactory
 import com.boozeblaster.models.Player
 
 fun main() {
-    val task = CommonTaskFactory().createTask().invoke(null, null, false)
-    val p1 = Player(name = "Mo")
-    val p2 = Player(name = "Mo2")
-    val versusTask = VersusTaskFactory().createTask().invoke(p1, p2, null)
-    val indTask = IndividualTaskFactory().createTask().invoke(p1, null, null)
-    println(task.getName())
-    println(versusTask.getName())
-    println(indTask.getName())
+
 }

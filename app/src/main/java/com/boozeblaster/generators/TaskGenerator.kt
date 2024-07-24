31e4ea097eef1f018211720d3aaabd002a7bab52
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2024-07-24 21:00:14 +0200

    remove unnecessary code

--- app/src/main/java/com/boozeblaster/generators/TaskGenerator.kt
+++ app/src/main/java/com/boozeblaster/generators/TaskGenerator.kt
@@ -4,13 +4,8 @@ import com.boozeblaster.generators.taskFactories.CommonTaskFactory
 import com.boozeblaster.generators.taskFactories.IndividualTaskFactory
 import com.boozeblaster.generators.taskFactories.TaskFactory
 import com.boozeblaster.generators.taskFactories.VersusTaskFactory
-import com.boozeblaster.minigames.common.SipTransfer
 import com.boozeblaster.models.Player
 import com.boozeblaster.tasks.Task
-import com.boozeblaster.tasks.common.CommonTask
-import com.boozeblaster.tasks.common.SipTransferTask
-import com.boozeblaster.tasks.individual.IndividualTask
-import com.boozeblaster.tasks.versus.VersusTask
 import com.boozeblaster.utils.GameSettings
 import kotlin.random.Random
 
@@ -79,85 +74,4 @@ object TaskGenerator {
         versusPlayer: Player? = null,
         generateSipTransferTask: Boolean? = null
     ): Task = taskFactory.createTask(player, versusPlayer, generateSipTransferTask)
-
-    /**
-     * Generates a random common task
-     * @param generateSipTransferTask Whether we want to generate a sip transfer task
-     * @return CommonTask (Specific common task - Or a SipTransferTask if we are in the last round)
-     * @see TaskGenerator.generateIndividualTask
-     * @see CommonTask
-     */
-    private fun generateCommonTask(generateSipTransferTask: Boolean): CommonTask {
-        if (generateSipTransferTask) {
-            return SipTransferTask(subTasks = listOf(SipTransfer()))
-        }
-        val game = getRandomTask(availableTasks = GameSettings.getCommonTasks())
-
-        val constructor = Class.forName("com.boozeblaster.tasks.common.${game}Task")
-            .getConstructor(List::class.java)
-
-        val instance = Class.forName("com.boozeblaster.generators.common.${game}Generator")
-            .newInstance()
-
-        val generator = Class.forName("com.boozeblaster.generators.common.${game}Generator")
-            .getMethod("getList")
-
-        return constructor.newInstance(generator.invoke(instance)) as CommonTask
-    }
-
-    /**
-     * This method generates a random individual task and assigns it to a player
-     *
-     * First, we generate a random index and take the string from the array INDIVIDUAL_TASKS
-     *
-     * Then, get the constructor of the class for the specific Task, and an instance of the matching
-     * generator with its getList() method
-     *
-     * We return a new instance of the given Task and initialize it with the passed argument Player
-     * and the return value of the getList() method
-     * @param player Player
-     * @return IndividualTask
-     * @see IndividualTask
-     */
-    private fun generateIndividualTask(player: Player): IndividualTask {
-        val game = getRandomTask(availableTasks = GameSettings.getIndividualTasks())
-
-        val constructor = Class.forName("com.boozeblaster.tasks.individual.${game}Task")
-            .getConstructor(Player::class.java, List::class.java)
-
-        val instance = Class.forName("com.boozeblaster.generators.individual.${game}Generator")
-            .newInstance()
-
-        val generator = Class.forName("com.boozeblaster.generators.individual.${game}Generator")
-            .getMethod("getList")
-
-        return constructor.newInstance(player, generator.invoke(instance)) as IndividualTask
-    }
-
-    /**
-     * Generates a random VersusTask
-     * @see VersusTask
-     */
-    private fun generateVersusTask(player: Player, versusPlayer: Player): VersusTask {
-        val game = getRandomTask(availableTasks = GameSettings.getVersusTasks())
-
-        val constructor = Class.forName("com.boozeblaster.tasks.versus.${game}Task")
-            .getConstructor(Player::class.java, List::class.java, Player::class.java)
-
-        val instance = Class.forName("com.boozeblaster.generators.versus.${game}Generator")
-            .newInstance()
-
-        val generator = Class.forName("com.boozeblaster.generators.versus.${game}Generator")
-            .getMethod("getList")
-
-        return constructor.newInstance(
-            player,
-            generator.invoke(instance),
-            versusPlayer
-        ) as VersusTask
-    }
-
-    private fun getRandomTask(availableTasks: Array<String>): String {
-        return availableTasks[Random.nextInt(from = 0, until = availableTasks.size)]
-    }
 }

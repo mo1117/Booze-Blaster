a971acbc24ea7770fdc9dca04f271c68e43d73aa
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2024-08-09 20:10:13 +0200

    decorator

---
+++ app/src/main/java/com/boozeblaster/composables/decorator/ClickableSurfaceDecorator.kt
@@ -0,0 +1,12 @@
+package com.boozeblaster.composables.decorator
+
+import androidx.compose.runtime.Composable
+
+object ClickableSurfaceDecorator {
+
+    @Composable
+    fun Decorate(surface: @Composable () -> Unit) {
+
+    }
+
+}
